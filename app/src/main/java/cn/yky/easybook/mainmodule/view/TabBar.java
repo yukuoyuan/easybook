e8045ba941ef604decc522c6b5b70f6cc55abce4
于阔源 <yukuoyuan@hotmail.com>
2017-04-24 16:46:09 +0800

    添加了底层的导航栏按钮的实现

--- app/src/main/java/cn/yky/easybook/mainmodule/view/TabBar.java
+++ app/src/main/java/cn/yky/easybook/mainmodule/view/TabBar.java
@@ -7,6 +7,7 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import cn.yky.easybook.R;
 
@@ -15,9 +16,28 @@ import cn.yky.easybook.R;
  * 这是一个自定义的底部导航栏的按钮
  */
 
-public class TabBar extends LinearLayout {
+public class TabBar extends LinearLayout implements View.OnClickListener {
 
     private LinearLayout mContentView;
+    private LinearLayout ll_tab_bar_home;
+    private LinearLayout ll_tab_bar_find;
+    private LinearLayout ll_tab_bar_write_article;
+    private LinearLayout ll_tab_bar_msg;
+    private LinearLayout ll_tab_bar_mine;
+    private TextView tv_tab_bar_home;
+    private TextView tv_tab_bar_find;
+    private TextView tv_tab_bar_write_article;
+    private TextView tv_tab_bar_msg;
+    private TextView tv_tab_bar_mine;
+    private OnTabCheckListener onTabCheckListener;
+
+    public interface OnTabCheckListener {
+        void onTabChecked(int position);
+    }
+
+    public void setTabCheckedListener(OnTabCheckListener onTabCheckListener) {
+        this.onTabCheckListener = onTabCheckListener;
+    }
 
     public TabBar(Context context) {
         super(context);
@@ -45,11 +65,116 @@ public class TabBar extends LinearLayout {
         //设置内容距离布局的边界的尺寸
         this.setPadding(0, 0, 0, 0);
         //添加内容布局并且设置宽是屏幕的宽.高为0
-        addView(mContentView, new LayoutParams(LayoutParams.MATCH_PARENT, 0));
+        addView(mContentView, new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
         //设置内容位于布局的下方
         setGravity(Gravity.BOTTOM);
         //测量
         measure(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        findview();
+        initListener();
+
     }
 
+    /**
+     * 设置监听
+     */
+    private void initListener() {
+        ll_tab_bar_home.setOnClickListener(this);
+        ll_tab_bar_find.setOnClickListener(this);
+        ll_tab_bar_write_article.setOnClickListener(this);
+        ll_tab_bar_msg.setOnClickListener(this);
+        ll_tab_bar_mine.setOnClickListener(this);
+    }
+
+    /**
+     * 初始化控件
+     */
+    private void findview() {
+        ll_tab_bar_home = (LinearLayout) mContentView.findViewById(R.id.ll_tab_bar_home);
+        ll_tab_bar_find = (LinearLayout) mContentView.findViewById(R.id.ll_tab_bar_find);
+        ll_tab_bar_write_article = (LinearLayout) mContentView.findViewById(R.id.ll_tab_bar_write_article);
+        ll_tab_bar_msg = (LinearLayout) mContentView.findViewById(R.id.ll_tab_bar_msg);
+        ll_tab_bar_mine = (LinearLayout) mContentView.findViewById(R.id.ll_tab_bar_mine);
+
+        tv_tab_bar_home = (TextView) mContentView.findViewById(R.id.tv_tab_bar_home);
+        tv_tab_bar_find = (TextView) mContentView.findViewById(R.id.tv_tab_bar_find);
+        tv_tab_bar_write_article = (TextView) mContentView.findViewById(R.id.tv_tab_bar_write_article);
+        tv_tab_bar_msg = (TextView) mContentView.findViewById(R.id.tv_tab_bar_msg);
+        tv_tab_bar_mine = (TextView) mContentView.findViewById(R.id.tv_tab_bar_mine);
+    }
+
+    @Override
+    public void onClick(View v) {
+        clearAllStatus();
+        switch (v.getId()) {
+            case R.id.ll_tab_bar_home:
+                tv_tab_bar_home.setEnabled(true);
+                setCheckedListener(0);
+                break;
+            case R.id.ll_tab_bar_find:
+                tv_tab_bar_find.setEnabled(true);
+                setCheckedListener(1);
+                break;
+            case R.id.ll_tab_bar_write_article:
+                tv_tab_bar_write_article.setEnabled(true);
+                setCheckedListener(2);
+                break;
+            case R.id.ll_tab_bar_msg:
+                tv_tab_bar_msg.setEnabled(true);
+                setCheckedListener(3);
+                break;
+            case R.id.ll_tab_bar_mine:
+                tv_tab_bar_mine.setEnabled(true);
+                setCheckedListener(4);
+                break;
+
+        }
+    }
+
+    /**
+     * 设置按钮的选中状态
+     */
+    public void setCheckedListener(int position) {
+        if (onTabCheckListener != null) {
+            onTabCheckListener.onTabChecked(position);
+        }
+    }
+
+    /**
+     * 清除所有的状态
+     */
+    private void clearAllStatus() {
+        tv_tab_bar_home.setEnabled(false);
+        tv_tab_bar_find.setEnabled(false);
+        tv_tab_bar_write_article.setEnabled(false);
+        tv_tab_bar_msg.setEnabled(false);
+        tv_tab_bar_mine.setEnabled(false);
+    }
+
+    /**
+     * 设置某一个按钮的选中状态
+     *
+     * @param tabbatPosition 按钮的位置
+     */
+    private void setTabBarEnabled(int tabbatPosition) {
+        clearAllStatus();
+        switch (tabbatPosition) {
+            case 0:
+                tv_tab_bar_home.setEnabled(true);
+                break;
+            case 1:
+                tv_tab_bar_find.setEnabled(true);
+                break;
+            case 2:
+                tv_tab_bar_write_article.setEnabled(true);
+                break;
+            case 3:
+                tv_tab_bar_msg.setEnabled(true);
+                break;
+            case 4:
+                tv_tab_bar_mine.setEnabled(true);
+                break;
+
+        }
+    }
 }
