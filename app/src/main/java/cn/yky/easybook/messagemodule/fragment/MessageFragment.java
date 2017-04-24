6926d17774ee9bee7deea23e39a7e83926df7ae6
于阔源 <yukuoyuan@hotmail.com>
2017-04-24 20:39:55 +0800

    提交了消息列表界面的提交

--- app/src/main/java/cn/yky/easybook/messagemodule/fragment/MessageFragment.java
+++ app/src/main/java/cn/yky/easybook/messagemodule/fragment/MessageFragment.java
@@ -1,7 +1,12 @@
 package cn.yky.easybook.messagemodule.fragment;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
+import android.view.View;
+import android.widget.TextView;
 
+import butterknife.Bind;
+import butterknife.OnClick;
 import cn.yky.easybook.R;
 import cn.yky.easybook.commonmodule.fragment.BaseFragment;
 
@@ -11,14 +16,20 @@ import cn.yky.easybook.commonmodule.fragment.BaseFragment;
  */
 
 public class MessageFragment extends BaseFragment {
+
+    @Bind(R.id.tv_toolbar_title)
+    TextView tvToolbarTitle;
+
     @Override
     public void initdata(Bundle arguments) {
+
 
     }
 
     @Override
     public int getContentLayout() {
         return R.layout.fragment_message;
+
     }
 
     @Override
@@ -30,4 +41,44 @@ public class MessageFragment extends BaseFragment {
     protected void initToolbar() {
 
     }
+
+    @OnClick({R.id.iv_toolbar_right, R.id.ll_msg_comment, R.id.ll_msg_message, R.id.ll_msg_ask, R.id.ll_msg_like_star, R.id.ll_msg_like_follow, R.id.ll_msg_like_appreciate, R.id.ll_msg_like_othertip})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.iv_toolbar_right:
+                break;
+            case R.id.ll_msg_comment:
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                getActivity().recreate();
+                break;
+            case R.id.ll_msg_message:
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                getActivity().recreate();
+                break;
+            case R.id.ll_msg_ask:
+                break;
+            case R.id.ll_msg_like_star:
+                break;
+            case R.id.ll_msg_like_follow:
+                break;
+            case R.id.ll_msg_like_appreciate:
+                break;
+            case R.id.ll_msg_like_othertip:
+                break;
+        }
+    }
+//
+//    @OnClick({R.id.tv_toolbar_title, R.id.bt_test})
+//    public void onViewClicked(View view) {
+//        switch (view.getId()) {
+//            case R.id.tv_toolbar_title:
+//                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+//                getActivity().recreate();
+//                break;
+//            case R.id.bt_test:
+//                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+//                getActivity().recreate();
+//                break;
+//        }
+//    }
 }
